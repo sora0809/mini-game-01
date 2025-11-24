baa69ec58de434b9fe9af6c956adc96577eeaa98
G384692023 <134909051+G384692023@users.noreply.github.com>
2025-11-24 14:50:01 +0900

    json

---
+++ src/vendor/phaser.js
@@ -0,0 +1,4 @@
+import * as PhaserNamespace from 'https://cdn.jsdelivr.net/npm/phaser@3.80.1/dist/phaser.esm.js';
+
+export default PhaserNamespace;
+export * from 'https://cdn.jsdelivr.net/npm/phaser@3.80.1/dist/phaser.esm.js';
